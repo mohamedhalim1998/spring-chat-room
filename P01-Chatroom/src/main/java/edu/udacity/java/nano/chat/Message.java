96178cbcf2c029ab1839d025af379d275b7113e3
MohamedHalim <mohamedhalim1998@gmail.com>
2021-12-24 13:28:47 +0200

    ADD: implement message model

--- P01-Chatroom/src/main/java/edu/udacity/java/nano/chat/Message.java
+++ P01-Chatroom/src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -4,5 +4,58 @@ package edu.udacity.java.nano.chat;
  * WebSocket message model
  */
 public class Message {
-    // TODO: add message model.
+
+    private String username;
+    private String msg;
+    private String type;
+    private int onlineCount;
+    public static final String SPEAK = "SPEAK";
+    public static final String CONNECT = "CONNECT";
+    public static final String DISCONNECT = "DISCONNECT";
+
+    public Message() {
+    }
+
+    public Message(String username, String msg) {
+        this.username = username;
+        this.msg = msg;
+        this.type = SPEAK;
+    }
+
+    public Message(String type, int onlineCount) {
+        this.type = type;
+        this.onlineCount = onlineCount;
+    }
+
+    public int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public void setOnlineCount(int onlineCount) {
+        this.onlineCount = onlineCount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
